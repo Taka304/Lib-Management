09bc9d413ec44bf3f10f3e3e027b54dbdcc54023
Tranmyhan <67772342+Tranmyhan@users.noreply.github.com>
2020-07-27 16:21:09 +0700

    Update borrowBook.h

--- borrowBook.h
+++ borrowBook.h
@@ -1,13 +1,9 @@
 #pragma once
+#include "reader.h"
 #define MAX_RID 9
 #define MAX_BID 9
 #define DATE 11
-struct date
-{
-	int day;
-	int month;
-	int year;
-};
+
 struct ISBNNode
 {
 	char ISBN[MAX_BID];
@@ -20,8 +16,9 @@ struct ISBNList
 struct bobInfo
 {
 	char rID[MAX_RID];
-	char BoDate[DATE];
-	char exBackDate[DATE];
+	date borrowDate;
+	date exBackDate;
+	int amount;
 	ISBNList l;
 };
 struct bobNode
@@ -38,5 +35,8 @@ ISBNNode* createISBNNode(char* x);
 void addISBN(ISBNList& l, char* x);
 void ISBNList_in(ISBNList& l, int n);
 void createBoBook(bobInfo& b);
+void addBorrowBook(bobInfo b, FILE* f);
 void ISBNList_out(ISBNList l);
 void BoBookOut(bobInfo b);
+void readBorrowBook(bobInfo& b, FILE* f);
+ISBNNode* findTailISBNList(ISBNList l);
