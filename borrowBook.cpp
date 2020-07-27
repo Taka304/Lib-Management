a5fe105575fe8d4c33077a645ed713a8fa0cf9a6
Tranmyhan <67772342+Tranmyhan@users.noreply.github.com>
2020-07-27 16:20:33 +0700

    Update borrowBook.cpp

--- borrowBook.cpp
+++ borrowBook.cpp
@@ -15,7 +15,7 @@ ISBNNode * createISBNNode(char *x)
 	node->next = NULL;
 	return node;
 }
-//them node id sach
+//tao + them node id sach
 void addISBN(ISBNList& l, char *x)
 {
 	ISBNNode* node = createISBNNode(x);
@@ -24,14 +24,8 @@ void addISBN(ISBNList& l, char *x)
 		l.head = node;
 		return;
 	}
-	for (ISBNNode* p = l.head; p; p = p->next)
-	{
-		if (p->next == NULL)
-		{
-			p->next = node;
-			return;
-		}
-	}
+	ISBNNode *tail = findTailISBNList(l);
+	tail->next = node;
 }
 //nhap danh sach id sach
 void ISBNList_in(ISBNList& l, int n)
@@ -59,22 +53,68 @@ void createBoBook(bobInfo& b)
 {
 	cout << "Nhap ma doc gia: ";
 	cin >> b.rID;
-	cout << "Nhap ngay muon sach: ";
-	cin >> b.BoDate;
+	cout << "Nhap ngay thang nam muon sach: ";
+	cin >> b.borrowDate.day;
+	cin >> b.borrowDate.month;
+	cin >> b.borrowDate.year;
 	cout << "Ngay tra du kien: ";
-	cin >> b.exBackDate;
+	cin >> b.exBackDate.day;
+	cin >> b.exBackDate.month;
+	cin >> b.exBackDate.year;
 	init_ISBNList(b.l);
-	int n;
 	cout << "Nhap so luong sach muon: ";
-	cin >> n;
-	ISBNList_in(b.l, n);
+	cin >> b.amount;
+	ISBNList_in(b.l, b.amount);
+}
+//tim duoi ISBN list
+ISBNNode* findTailISBNList(ISBNList l)
+{
+	for (ISBNNode* p = l.head; p; p = p->next)
+	{
+		if (p->next == NULL)
+		{
+			return p;
+		}
+	}
+	return NULL;
+}
+//doc phieu muon tu file
+void readBorrowBook(bobInfo& b, FILE* f)
+{
+	fscanf(f, "%[^,]", b.rID);
+	fgetc(f);
+	readDate(f, b.borrowDate);
+	fgetc(f);
+	readDate(f, b.exBackDate);
+	fgetc(f);
+	fscanf(f, "%[^,]", &b.amount);
+	fgetc(f);
+	cout << b.amount;
+	for (int i = 0; i < b.amount; i++)
+	{
+		char id[MAX_BID];
+		fscanf(f, "%[^,]", id);
+		fgetc(f);
+		addISBN(b.l, id);
+	}
+}
+//ghi phieu muon vao file
+void addBorrowBook(bobInfo b, FILE *f)
+{
+	fprintf(f, "%s,%d/%d/%d,%d/%d/%d,%d", b.rID,b.borrowDate,b.exBackDate, b.amount);
+	for (ISBNNode* p = b.l.head; p; p = p->next)
+	{
+		fprintf(f, ",%s",p->ISBN);
+	}
+	fprintf(f, "\n");
 }
 //in phieu muon
 void BoBookOut(bobInfo b)
 {
 	cout << "Ma doc gia: " << b.rID << endl;
-	cout << "Ngay muon sach: " << b.BoDate << endl;
-	cout << "Ngay tra du kien: " << b.exBackDate << endl;
+	cout << "Ngay muon sach: " << b.borrowDate.day << "/" << b.borrowDate.month << "/" << b.borrowDate.year << endl;
+	cout << "Ngay tra du kien: " << b.exBackDate.day << "/" << b.exBackDate.month << "/" << b.exBackDate.year << endl;
+	cout << "So luong sach muon: " << b.amount << endl;
 	cout << "Danh sach sach muon: ";
 	ISBNList_out(b.l);
 }
